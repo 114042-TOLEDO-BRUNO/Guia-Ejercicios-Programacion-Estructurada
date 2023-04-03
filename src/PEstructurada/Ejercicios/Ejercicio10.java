3ead1a9eb190a3eea1038f4a138c8364069c534b
114042_TOLEDO_BRUNO <114042@tecnicatura.frc.utn.edu.ar>
2023-04-03 08:09:44 -0300

    "Completando ejercicio 10"

--- src/PEstructurada/Ejercicios/Ejercicio10.java
+++ src/PEstructurada/Ejercicios/Ejercicio10.java
@@ -1,7 +1,26 @@
 package PEstructurada.Ejercicios;
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
 public class Ejercicio10
 {
     //Ingresar un número y validar que sea positivo. Si no lo es continuar
     //solicitándolo al usuario hasta que ingrese un número correcto
+    public static void main(String[] args)
+    {
+        Scanner read=new Scanner(System.in);
+        int number;
+        System.out.println("Ingrese un numero");
+        number=read.nextInt();
+        while (number<=0)
+        {
+            System.out.println("Ingrese un numero valido");
+            number=read.nextInt();
+        }
+        System.out.println("El numero "+number+" es positivo");
+    }
+
+
 }
